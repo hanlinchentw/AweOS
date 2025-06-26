f949b7f4d9e3635bfdb594b5fa8f5f536ff0ecee
leoc <leoc@synology.com>
2025-06-26 17:55:13 +0800

    feat: print strings on screen

---
+++ src/drivers/screen.c
@@ -0,0 +1,156 @@
+#include "screen.h"
+#include "ports.h"
+#include "../kernel/utils.h"
+
+int get_cursor_offset();
+void set_cursor_offset(int offset);
+int print_char(char c, int col, int row, char attr);
+int get_offset(int col, int row);
+int get_offset_row(int offset);
+int get_offset_col(int offset);
+
+/*******************************
+ * Public Kernel API functions *
+ *******************************/
+
+void kprint_at(char *message, int col, int row)
+{
+   int offset;
+   if (col >= 0 && row >= 0)
+   {
+      offset = get_offset(col, row);
+   }
+   else
+   {
+      offset = get_cursor_offset();
+      col = get_offset_col(offset);
+      row = get_offset_row(offset);
+   }
+
+   /* Loop through message and print it */
+   int i = 0;
+   while (message[i] != 0)
+   {
+      offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
+      /* Compute row/col for next iteration */
+      row = get_offset_row(offset);
+      col = get_offset_col(offset);
+   }
+}
+
+void kprint(char *message)
+{
+   kprint_at(message, -1, -1);
+}
+
+/*******************************
+ * Private Kernel API functions *
+ ********************************/
+
+int print_char(char c, int col, int row, char attr)
+{
+   unsigned char *vidmem = (unsigned char *)VIDEO_ADDRESS; // 0xb8000
+   if (!attr)
+      attr = WHITE_ON_BLACK;
+
+   /* Error control: print a red 'E' if the coords aren't right */
+   if (col >= MAX_COLS || row >= MAX_ROWS)
+   {
+      vidmem[2 * (MAX_COLS) * (MAX_ROWS)-2] = 'E';
+      vidmem[2 * (MAX_COLS) * (MAX_ROWS)-1] = RED_ON_WHITE;
+      return get_offset(col, row);
+   }
+
+   int offset;
+   if (col >= 0 && row >= 0)
+      offset = get_offset(col, row);
+   else
+      offset = get_cursor_offset();
+
+   if (c == '\n')
+   {
+      row = get_offset_row(offset);
+      offset = get_offset(0, row + 1);
+   }
+   else
+   {
+      vidmem[offset] = c;
+      vidmem[offset + 1] = attr;
+      offset += 2;
+   }
+
+   /* Check if the offset is over screen size and scroll */
+   if (offset >= MAX_ROWS * MAX_COLS * 2)
+   {
+      int i;
+      for (i = 1; i < MAX_ROWS; i++) {
+         char *source = (char *)get_offset(0, i) + VIDEO_ADDRESS;
+         char *dest = (char *)get_offset(0, i - 1) + VIDEO_ADDRESS;
+         int nbytes = MAX_COLS * 2;
+         memory_copy(source, dest, nbytes);
+      }
+
+      /* Blank last line */
+      char *last_line = (char *)get_offset(0, MAX_ROWS - 1) + VIDEO_ADDRESS;
+      for (i = 0; i < MAX_COLS * 2; i++)
+         last_line[i] = 0;
+
+      offset -= 2 * MAX_COLS;
+   }
+
+   set_cursor_offset(offset);
+   return offset;
+}
+
+void set_cursor_offset(int offset)
+{
+   offset /= 2;
+   port_byte_out(REG_SCREEN_CTRL, 14);
+   port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset >> 8));
+   port_byte_out(REG_SCREEN_CTRL, 15);
+   port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
+}
+
+int get_cursor_offset()
+{
+   int position;
+
+   // get high byte
+   port_byte_out(REG_SCREEN_CTRL, 14);
+   position = port_byte_in(REG_SCREEN_DATA) << 8;
+
+   // get low byte
+   port_byte_out(REG_SCREEN_CTRL, 15);
+   position |= port_byte_in(REG_SCREEN_DATA);
+
+   return position * 2;
+}
+
+void clear_screen()
+{
+   int screen_size = MAX_COLS * MAX_ROWS;
+   int i;
+   char *screen = (unsigned char *)VIDEO_ADDRESS;
+
+   for (i = 0; i < screen_size; i++)
+   {
+      screen[i * 2] = ' ';
+      screen[i * 2 + 1] = WHITE_ON_BLACK;
+   }
+   set_cursor_offset(get_offset(0, 0));
+}
+
+int get_offset(int col, int row)
+{
+   return 2 * (row * MAX_COLS + col);
+}
+
+int get_offset_row(int offset)
+{
+   return offset / (2 * MAX_COLS);
+}
+
+int get_offset_col(int offset)
+{
+   return (offset - (get_offset_row(offset) * 2 * MAX_COLS)) / 2;
+}
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,19 +1,18 @@
 #include "../drivers/ports.h"
+#include "../drivers/screen.h"
+#include "utils.h"
 
-void main() 
+void main()
 {
-	/* Screen cursor position: ask VGA control register (0x3d4) for bytes
-     * 14 = high byte of cursor and 15 = low byte of cursor. */
-	port_byte_out(0x3d4, 14);  /* Requesting byte 14: high byte of cursor pos */
+    /* Fill up the screen */
+    int i = 0;
+    for (i = 0; i < 24; i++)
+    {
+        char str[255];
+        int_to_ascii(i, str);
+        kprint_at(str, 0, i);
+    }
 
-	int position = port_byte_in(0x3d5); /* Data is returned in VGA data register (0x3d5) */
-	position = position << 8; /* high byte, e.g, 0x10 << 8 = 0x1000 */
-
-	port_byte_out(0x3d4, 15); /* requesting low byte */
-	position |= port_byte_in(0x3d5); /* add low byte, 0x1000 | 0x2C = 0x102C = Cursor cell index */
-
-	int offset_from_vga = position * 2; // Each character takes 2 bytes in VGA memory
-	char *vga = (char *) 0xb8000;
-	vga[offset_from_vga] = 'X';
-	vga[offset_from_vga + 1] = 0x0f; // White on black
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
+    kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
 }
